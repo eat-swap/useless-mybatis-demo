67cc7fb764149694f0f34c7775b96f151402d8bc
Eatswap <root@eatswap.org>
2022-11-02 04:01:18 +0800

    refactor: move controller

--- src/main/java/tk/haunt/mybatis/modules/blog/BlogController.java
+++ src/main/java/tk/haunt/mybatis/modules/blog/BlogController.java
@@ -1,27 +1,24 @@
-package tk.haunt.mybatis.modules.post;
+package tk.haunt.mybatis.modules.blog;
 
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import tk.haunt.mybatis.modules.author.Author;
-import tk.haunt.mybatis.modules.author.AuthorMapper;
-import tk.haunt.mybatis.modules.blog.Blog;
-import tk.haunt.mybatis.modules.blog.BlogMapper;
 
 @RestController
-@RequestMapping("/v1/posts")
-public class PostController {
+@RequestMapping("/v1/blog")
+public class BlogController {
 
     @Autowired
     private SqlSessionFactory sqlSessionFactory;
 
-    @GetMapping("/test")
-    public Blog test() {
+    @GetMapping("/{id}")
+    public Blog test(@PathVariable long id) {
         try (var s = sqlSessionFactory.openSession()) {
-            var ret = s.getMapper(BlogMapper.class).findBlogById(1);
-            return ret;
+            return s.getMapper(BlogMapper.class).findBlogById(id);
         }
     }
 
